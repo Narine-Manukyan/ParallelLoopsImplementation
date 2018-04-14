1e653eab76d3d2d04b5e8cd4185a2ba14045e9c3
Nar <Nar@NAR->
2018-04-15 02:07:05 +0400

    Add project files.

---
+++ ParellelLoops/MyParallel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParellelLoops
+{
+    /// <summary>
+    /// Provides support for parallel loops and regions.
+    /// </summary>
+    public static class MyParallel
+    {
+        /// <summary>
+        /// Executes a for loop in which iterations may run in parallel.
+        /// </summary>
+        /// <param name="fromInclusive">The start index(inclusive)</param>
+        /// <param name="toExclusive">The end index (exclusive)</param>
+        /// <param name="body">The delegate that is invoked once per iteration.<param>
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            if (body == null)
+                throw new ArgumentNullException("Body is Null.");
+            // If 'to' is less than 'from'.
+            if (toExclusive <= fromInclusive)
+                return;
+            // List for tasks.
+            List<Task> tasks = new List<Task>();
+            // Current position.
+            int j = fromInclusive;
+            for (int i = fromInclusive; i < toExclusive; i++)
+            {
+                // Starting new Task.
+                Task t = Task.Run(() => body(j++));
+                tasks.Add(t);
+            }
+            // Wait for each task parallel.
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        /// <summary>
+        /// Executes a foreach operation on an IEnumerable in which iterations may run in parallel, 
+        /// loop options can be configured, and the state of the loop can be monitored and manipulated.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            if(body == null)
+                throw new ArgumentNullException("Body is Null.");
+            if(source == null)
+                throw new ArgumentNullException("Source is Null.");
+            // List for tasks.
+            List<Task> tasks = new List<Task>();
+            foreach (TSource item in source)
+            {
+                // Starting new Task.
+                Task t = Task.Run(() => body(item));
+                tasks.Add(t);
+            }
+            // Wait for each task parallel.
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        /// <summary>
+        /// Executes a foreach operation on an IEnumerable in which iterations may run in parallel and loop options can be configured.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="parallelOptions">An object that configures the behavior of this operation.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source,
+            ParallelOptions parallelOptions, Action<TSource> body)
+        {
+            if (body == null)
+                throw new ArgumentNullException("Body is Null.");
+            if (source == null)
+                throw new ArgumentNullException("Source is Null.");
+            if (parallelOptions == null)
+                throw new ArgumentNullException("ParallelOptions is Null.");
+            if (parallelOptions.MaxDegreeOfParallelism <= -1)
+                throw new Exception("MaxDegreeOfParallelism is less than -1");
+            // List for tasks.
+            // MaxDegreeOfParallelism Gets or Sets the maximum number of concurrent tasks 
+            // enabled by this ParallelOptions instance.
+            List<Task> tasks = new List<Task>();
+            int j = 0;
+            foreach (TSource item in source)
+            {
+                // Starting new Task.
+                Task t = Task.Run(() => body(item));
+                tasks.Add(t);
+                j = tasks.IndexOf(t);
+                if (j >= parallelOptions.MaxDegreeOfParallelism)
+                {
+                    Task.WaitAny(tasks.ToArray());
+                }
+                j++;
+            }
+            // Wait for each task parallel.
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
---
+++ ParellelLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParellelLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Testing ParallelFor.
+            //MyParallel.ParallelFor(0, 100, index => {
+            //    Console.WriteLine("Task ID {0} processing index: {1}",
+            //    Task.CurrentId, index);
+            //});
+
+            //// Testing ParallelForEach.
+            //MyParallel.ParallelForEach(
+            //    new List<string> { "apple", "blue", "Hello", "cotton", "dark", "fox" },
+            //    item => 
+            //    {
+            //        Console.WriteLine("Item {0} has {1} characters",
+            //        item, item.Length);
+            //    });
+
+            //Testing ParallelForEachWithOptions.
+            MyParallel.ParallelForEachWithOptions(new List<int> { 1, 2, 3, 4, 5, 6, 7 },
+                new ParallelOptions { MaxDegreeOfParallelism = 3 },
+                value =>
+                {
+                    Console.WriteLine("Task ID: {0},\tValue {1} ", Task.CurrentId, value);
+                });
+
+            //Parallel.ForEach(new List<int> { 1, 2, 3, 4, 5, 6, 7 },
+            //    new ParallelOptions { MaxDegreeOfParallelism = 3},
+            //    index =>
+            //    {
+            //        Console.WriteLine("Task ID: {0}, Value {1} ", Task.CurrentId, index);
+            //    });
+        }
+    }
+}
